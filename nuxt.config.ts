ce5273cf911d38574897f045e6dfe6922520110a
Cloudea <cloudeasoft@qq.com>
2026-08-03 18:21:04 +0800

    chore: update Nuxt compatibility date (#7)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-12-20',
+	compatibilityDate: '2026-08-03',
 	devtools: {
 		enabled: true,
 		componentInspector: false,
